e39a292fc18f45a629b19d29a08e0352d0602619
seancarroll <seanc28@gmail.com>
2019-01-28 23:06:33 -0600

    add logic for EndsWithConstraint

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/EndsWithConstraint.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/EndsWithConstraint.java
@@ -7,11 +7,12 @@ public class EndsWithConstraint implements Constraint<CharSequence> {
     private final CharSequence suffix;
 
     public EndsWithConstraint(CharSequence suffix) {
+        // TODO: null check
         this.suffix = suffix;
     }
 
     @Override
     public boolean isValid(CharSequence instance) {
-        return false;
+        return instance.toString().endsWith(suffix.toString());
     }
 }
