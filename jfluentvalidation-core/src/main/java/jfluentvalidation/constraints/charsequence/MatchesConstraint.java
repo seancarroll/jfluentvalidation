294dc5177725e95e3e5ad26a2a06c7bcc53cacf1
seancarroll <seanc28@gmail.com>
2019-01-30 21:32:23 -0600

    support both CharSequence and pattern

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/MatchesConstraint.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/MatchesConstraint.java
@@ -2,18 +2,24 @@ package jfluentvalidation.constraints.charsequence;
 
 import jfluentvalidation.constraints.Constraint;
 
+import java.util.regex.Pattern;
+
 // TODO: given we have multiple of these based on type need to either change the name to include the type we are comparing
 // or I guess we could make this take an object and have logic to determine type and perform the appropriate validation
 public class MatchesConstraint implements Constraint<CharSequence> {
 
-    private final CharSequence regex;
+    private final Pattern pattern;
 
     public MatchesConstraint(CharSequence regex) {
-        this.regex = regex;
+        this(Pattern.compile(regex.toString()));
+    }
+
+    public MatchesConstraint(Pattern pattern) {
+        this.pattern = pattern;
     }
 
     @Override
     public boolean isValid(CharSequence instance) {
-        return false;
+        return pattern.matcher(instance).matches();
     }
 }
