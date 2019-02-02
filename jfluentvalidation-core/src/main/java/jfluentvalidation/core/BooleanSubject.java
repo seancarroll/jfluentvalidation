eb0ec38b0c12862cc78ade1f6aab35527f091c27
seancarroll <seanc28@gmail.com>
2019-02-01 19:47:42 -0600

    adds BooleanSubject

---
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/BooleanSubject.java
@@ -0,0 +1,71 @@
+package jfluentvalidation.core;
+
+import java.util.function.Function;
+
+public class BooleanSubject extends Subject<BooleanSubject, Boolean> implements ComparableSubject<BooleanSubject, Boolean> {
+
+    public BooleanSubject(Function propertyFunc, String propertyName) {
+        super(BooleanSubject.class, propertyFunc, propertyName);
+    }
+
+    // TODO: isEqual and isNotEqual
+    public BooleanSubject isTrue() {
+        // TODO: should we just use a isEquals(true) constraint or have a IsTrue() constraint?
+        //constraints.add(IsEqualsConstraint(true));
+        isEquals(true);
+        return myself;
+    }
+
+    public BooleanSubject isFalse() {
+        // TODO: should we just use a isEquals(false) constraint or have a IsFalse() constraint?
+        isEquals(false);
+        return myself;
+    }
+
+    // TODO: do these makes sense for boolean? While boolean does implement Comparable perhaps here is another way to
+    // split compareTo from these others? Maybe they belong to ComparableNumber?
+    @Override
+    public BooleanSubject isEqualAccordingToCompareTo(Boolean other) {
+        return null;
+    }
+
+    @Override
+    public BooleanSubject isNotEqualAccordingToCompareTo(Boolean other) {
+        return null;
+    }
+
+    @Override
+    public BooleanSubject isLessThan(Boolean other) {
+        return null;
+    }
+
+    @Override
+    public BooleanSubject isLessThanOrEqualTo(Boolean other) {
+        return null;
+    }
+
+    @Override
+    public BooleanSubject isGreaterThan(Boolean other) {
+        return null;
+    }
+
+    @Override
+    public BooleanSubject isGreaterThanOrEqualTo(Boolean other) {
+        return null;
+    }
+
+    @Override
+    public BooleanSubject isBetween(Boolean startInclusive, Boolean endInclusive) {
+        return null;
+    }
+
+    @Override
+    public BooleanSubject isStrictlyBetween(Boolean startExclusive, Boolean endExclusive) {
+        return null;
+    }
+
+    @Override
+    public BooleanSubject isBetween(Boolean start, Boolean end, boolean inclusiveStart, boolean inclusiveEnd) {
+        return null;
+    }
+}
