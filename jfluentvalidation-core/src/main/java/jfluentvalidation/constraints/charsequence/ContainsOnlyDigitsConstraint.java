861daae8c5874d7d1093d518e33bfc8e3636c291
seancarroll <seanc28@gmail.com>
2019-02-16 20:49:00 -0600

    adding more constraints

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/ContainsOnlyDigitsConstraint.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/ContainsOnlyDigitsConstraint.java
@@ -1,5 +1,6 @@
 package jfluentvalidation.constraints.charsequence;
 
+import jfluentvalidation.common.Strings;
 import jfluentvalidation.constraints.Constraint;
 
 public class ContainsOnlyDigitsConstraint implements Constraint<CharSequence> {
@@ -7,9 +8,13 @@ public class ContainsOnlyDigitsConstraint implements Constraint<CharSequence> {
     // TODO: null/empty
     @Override
     public boolean isValid(CharSequence instance) {
-//        if (isEmpty(cs)) {
-//            return false;
-//        }
+        if (Strings.isNullOrEmpty(instance)) {
+            return false;
+        }
+
+        // instance.chars().allMatch(Character::isDigit)
+
+        // TODO: should we return index of non-digit characters?
         for (int i = 0; i < instance.length(); i++) {
             if (!Character.isDigit(instance.charAt(i))) {
                 return false;
--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/ContainsOnlyOnceConstraint.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/ContainsOnlyOnceConstraint.java
@@ -1,37 +1,58 @@
 package jfluentvalidation.constraints.charsequence;
 
+import jfluentvalidation.common.Strings;
 import jfluentvalidation.constraints.Constraint;
+import jfluentvalidation.internal.Ensure;
 
+// TODO: allow custom comparison strategy
 public class ContainsOnlyOnceConstraint implements Constraint<CharSequence> {
 
     private final CharSequence sequence;
 
     public ContainsOnlyOnceConstraint(CharSequence sequence) {
-        this.sequence = sequence;
+        this.sequence = Ensure.notNull(sequence);
     }
 
     // TODO: update so not completely lang-commons
     // TODO: should we just bring in commons-lang????
     @Override
     public boolean isValid(CharSequence instance) {
-        int lastIndex = 0;
-        int count = 0;
-
-        // TODO: Improve...I'm sure this can be better
-        String sequenceAsString = sequence.toString();
-        String instanceAsString = instance.toString();
-        while (lastIndex != -1) {
-            lastIndex = instanceAsString.indexOf(sequenceAsString, lastIndex);
-
-            if (lastIndex != -1) {
-                count++;
-                if (count >= 2) {
-                    return false;
-                }
-                lastIndex += sequenceAsString.length();
-            }
+        if (Strings.isNull(instance)) {
+            return false;
         }
 
-        return true;
+        // TODO: do we want to count all instances?
+        int count = 0;
+        int fromIndex = 0;
+        String sequenceAsString = sequence.toString();
+        String instanceAsString = instance.toString();
+        while ((fromIndex = instanceAsString.indexOf(sequenceAsString, fromIndex)) != -1 ) {
+            count++;
+            if (count >= 2) {
+                return false;
+            }
+            fromIndex += sequenceAsString.length();
+        }
+
+
+
+//        int lastIndex = 0;
+//        int count = 0;
+//
+//        // TODO: Improve...I'm sure this can be better
+//        String sequenceAsString = sequence.toString();
+//        String instanceAsString = instance.toString();
+//        while (lastIndex != -1) {
+//            lastIndex = instanceAsString.indexOf(sequenceAsString, lastIndex);
+//            if (lastIndex != -1) {
+//                count++;
+//                if (count >= 2) {
+//                    return false;
+//                }
+//                lastIndex += sequenceAsString.length();
+//            }
+//        }
+
+        return count == 1;
     }
 }
