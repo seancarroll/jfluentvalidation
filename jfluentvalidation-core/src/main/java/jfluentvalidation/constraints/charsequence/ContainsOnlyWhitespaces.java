4d068042fa5c419f160b3a72f24fe364342f59a9
seancarroll <seanc28@gmail.com>
2019-02-16 20:20:08 -0600

    implemnt charsequence ContainsOnlyWhitespaces

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/ContainsOnlyWhitespaces.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/ContainsOnlyWhitespaces.java
@@ -1,11 +1,12 @@
 package jfluentvalidation.constraints.charsequence;
 
+import jfluentvalidation.common.Strings;
 import jfluentvalidation.constraints.Constraint;
 
 public class ContainsOnlyWhitespaces implements Constraint<CharSequence> {
 
     @Override
     public boolean isValid(CharSequence instance) {
-        return false;
+        return Strings.isNullOrEmpty(instance) && instance.chars().allMatch(Character::isWhitespace);
     }
 }
