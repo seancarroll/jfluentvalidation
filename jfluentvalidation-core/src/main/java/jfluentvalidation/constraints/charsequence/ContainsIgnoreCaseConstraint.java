35590cacd6b01d0559ecf140bd6ee2b15b3b040e
seancarroll <seanc28@gmail.com>
2019-02-16 20:19:30 -0600

    implement

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/ContainsIgnoreCaseConstraint.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/ContainsIgnoreCaseConstraint.java
@@ -1,17 +1,18 @@
 package jfluentvalidation.constraints.charsequence;
 
 import jfluentvalidation.constraints.Constraint;
+import jfluentvalidation.internal.Ensure;
 
 public class ContainsIgnoreCaseConstraint implements Constraint<CharSequence> {
 
     private final CharSequence charSequence;
 
     public ContainsIgnoreCaseConstraint(CharSequence charSequence) {
-        this.charSequence = charSequence;
+        this.charSequence = Ensure.notNull(charSequence);
     }
 
     @Override
     public boolean isValid(CharSequence instance) {
-        return false;
+        return instance.toString().toLowerCase().contains(charSequence.toString().toLowerCase());
     }
 }
