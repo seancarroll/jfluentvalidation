98daee07f882c5256cfe708af77b2a2614a03c4d
seancarroll <seanc28@gmail.com>
2019-02-16 20:15:06 -0600

    adding javadocs

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/Constraint.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/Constraint.java
@@ -1,7 +1,17 @@
 package jfluentvalidation.constraints;
 
+/**
+ * Defines the logic to validate a given constraint for a given object type {@code T}
+ * @param <T> the target type supported by an implementation
+ */
 public interface Constraint<T> {
 
-    boolean isValid(T instance);
+    /**
+     * Implements the validation logic.
+     *
+     * @param value object to validate
+     * @return {@code false} if {@code value} does not pass the constraint
+     */
+    boolean isValid(T value);
 
 }
