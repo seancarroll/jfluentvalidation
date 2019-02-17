8d530bc14ad404128bbd052b0e2d1d1fa5d8ee2e
seancarroll <seanc28@gmail.com>
2019-02-16 23:46:50 -0600

    adds containsWhitespaces and containsOnlyWhitespace methods

--- jfluentvalidation-core/src/main/java/jfluentvalidation/common/Strings.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/common/Strings.java
@@ -4,12 +4,19 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class Strings {
 
-
     public static boolean isNull(@Nullable CharSequence charSequence) {
         return charSequence == null;
     }
 
     public static boolean isNullOrEmpty(@Nullable CharSequence charSequence) {
         return charSequence == null || charSequence.toString().isEmpty();
+    }
+
+    public static boolean containsWhitespaces(CharSequence value) {
+        return value.chars().anyMatch(Character::isWhitespace);
+    }
+
+    public static boolean containsOnlyWhitespace(CharSequence value) {
+        return value.chars().allMatch(Character::isWhitespace);
     }
 }
