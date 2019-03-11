c133a1c4ea8a6fdf5b96d4f03c6ba3bd27ca8e63
seancarroll <seanc28@gmail.com>
2019-03-10 22:10:23 -0500

    fix generics in ComparableConstraints

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/comparable/ComparableConstraints.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/comparable/ComparableConstraints.java
@@ -3,35 +3,35 @@ package jfluentvalidation.constraints.comparable;
 // TODO: should we use generics?
 public final class ComparableConstraints {
 
-    public static <T extends Comparable<? super T>> IsBetweenConstraint isBetween(Comparable<T> start, Comparable<T> end, boolean inclusiveStart, boolean inclusiveEnd) {
+    public static <T extends Comparable<? super T>> IsBetweenConstraint isBetween(T start, T end, boolean inclusiveStart, boolean inclusiveEnd) {
         return new IsBetweenConstraint(start, end, inclusiveStart, inclusiveEnd);
     }
 
-    public static <T extends Comparable<? super T>> IsEqualAccordingToCompareToConstraint isEqualAccordingToCompareTo(Comparable<T> other) {
+    public static <T extends Comparable<? super T>> IsEqualAccordingToCompareToConstraint isEqualAccordingToCompareTo(T other) {
         return new IsEqualAccordingToCompareToConstraint(other);
     }
 
-    public static <T extends Comparable<? super T>> IsGreaterThanConstraint isGreaterThan(Comparable<T> other) {
+    public static <T extends Comparable<? super T>> IsGreaterThanConstraint isGreaterThan(T other) {
         return new IsGreaterThanConstraint(other);
     }
 
-    public static <T extends Comparable<? super T>> IsGreaterThanOrEqualToConstraint isGreaterThanOrEqualTo(Comparable<T> other) {
+    public static <T extends Comparable<? super T>> IsGreaterThanOrEqualToConstraint isGreaterThanOrEqualTo(T other) {
         return new IsGreaterThanOrEqualToConstraint(other);
     }
 
-    public static <T extends Comparable<? super T>> IsLessThanConstraint isLessThan(Comparable<T> other) {
+    public static <T extends Comparable<? super T>> IsLessThanConstraint isLessThan(T other) {
         return new IsLessThanConstraint(other);
     }
 
-    public static <T extends Comparable<? super T>> IsLessThanOrEqualToConstraint isLessThanOrEqualTo(Comparable<T> other) {
+    public static <T extends Comparable<? super T>> IsLessThanOrEqualToConstraint isLessThanOrEqualTo(T other) {
         return new IsLessThanOrEqualToConstraint(other);
     }
 
-    public static <T extends Comparable<? super T>> IsNotBetweenConstraint isNotBetween(Comparable<T> start, Comparable<T> end, boolean inclusiveStart, boolean inclusiveEnd) {
+    public static <T extends Comparable<? super T>> IsNotBetweenConstraint isNotBetween(T start, T end, boolean inclusiveStart, boolean inclusiveEnd) {
         return new IsNotBetweenConstraint(start, end, inclusiveStart, inclusiveEnd);
     }
 
-    public static <T extends Comparable<? super T>> IsNotEqualAccordingToCompareToConstraint isNotEqualAccordingToCompareTo(Comparable<T> other) {
+    public static <T extends Comparable<? super T>> IsNotEqualAccordingToCompareToConstraint isNotEqualAccordingToCompareTo(T other) {
         return new IsNotEqualAccordingToCompareToConstraint(other);
     }
 
