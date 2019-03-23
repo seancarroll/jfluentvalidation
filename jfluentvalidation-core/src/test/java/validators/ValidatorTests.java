11102c35885825ccd23943f93cdeabaaba94f2df
seancarroll <seanc28@gmail.com>
2019-03-23 11:10:32 -0500

    bit of ValidatorTests clean up

--- jfluentvalidation-core/src/test/java/validators/ValidatorTests.java
+++ jfluentvalidation-core/src/test/java/validators/ValidatorTests.java
@@ -32,20 +32,7 @@ class ValidatorTests {
         Person m = new Person("sean", 32, "");
         m.setChilren(Arrays.asList("Shawn", "Sally"));
 
-        // TODO: better way than creating a validator like this?
-        // the fluent validator approach might be a good way to do it
-        // although it might be nice to be able to create a validator without having a custom validator class
-        // that we need to instantiate
         PersonValidator validator = new PersonValidator();
-
-        validator.ruleForString(p -> p.getName()).isEmpty().startsWith("s").length(0, 4);
-        validator.ruleForString(p -> p.getAddress()).isNotNull();
-        validator.ruleForInteger(p -> p.getAge()).isPositive();
-        validator.ruleForBoolean(p -> p.isMarried()).isFalse();
-        validator.ruleForZonedDateTime(p -> p.getSignedIn()).isAfter(ZonedDateTime.now().minusDays(1));
-        validator.ruleForMap(p -> p.getPets()).isEmpty();
-        validator.ruleForIterable(p -> p.getChilren()).isNotNull().isNotEmpty();
-
 
         List<ValidationFailure> validationFailures = validator.validate(m);
 
@@ -55,9 +42,13 @@ class ValidatorTests {
     private class PersonValidator extends AbstractValidator<Person> {
 
         protected PersonValidator() {
-            // TODO: how do we think we should implement a way to add constraints for each item in a collection?
-            // One idea...
-            // ruleForIterable(p -> p.getChilren()).forEach(startsWith("S"));
+            ruleForString(p -> p.getName()).isEmpty().startsWith("s").length(0, 4);
+            ruleForString(p -> p.getAddress()).isNotNull();
+            ruleForInteger(p -> p.getAge()).isPositive();
+            ruleForBoolean(p -> p.isMarried()).isFalse();
+            ruleForZonedDateTime(p -> p.getSignedIn()).isAfter(ZonedDateTime.now().minusDays(1));
+            ruleForMap(p -> p.getPets()).isEmpty();
+            ruleForIterable(p -> p.getChilren()).isNotNull().isNotEmpty();
         }
     }
 
