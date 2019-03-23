196f7515206cc61a5e3858c71b1c49948ad06fd7
seancarroll <seanc28@gmail.com>
2019-03-23 11:05:21 -0500

    adds an empty List<ValidationFailure>

--- jfluentvalidation-core/src/main/java/jfluentvalidation/internal/Empty.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/internal/Empty.java
@@ -1,8 +1,16 @@
 package jfluentvalidation.internal;
 
+import jfluentvalidation.ValidationFailure;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Empty {
 
     public static final String[] STRING_ARRAY = new String[0];
+
+    // TODO: should we just use Collections.EMPTY_LIST; here?
+    public static final List<ValidationFailure> VALIDATION_FAILURES = new ArrayList<>(0);
 
     private Empty() {
         // statics only
