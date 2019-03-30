a338858a2f608344f0fd927c48768e85ab8d3592
seancarroll <seanc28@gmail.com>
2019-03-30 15:45:37 -0500

    oops didnt include IterableItemConstraint in the last commit

--- jfluentvalidation-core/src/main/java/jfluentvalidation/core/IterableSubject.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/IterableSubject.java
@@ -1,9 +1,10 @@
 package jfluentvalidation.core;
 
 import jfluentvalidation.constraints.Constraint;
+import jfluentvalidation.constraints.SoftConstraint;
 import jfluentvalidation.constraints.iterable.*;
-import jfluentvalidation.constraints.CollectionConstraint;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
@@ -12,8 +13,9 @@ import java.util.function.Predicate;
 import static com.google.common.collect.Lists.newArrayList;
 import static java.util.Arrays.asList;
 
-// QUESTION: should we add generic T?
 public class IterableSubject<T> extends Subject<IterableSubject<T>, Iterable<T>> {
+
+    protected List<Constraint<? super T>> itemConstraints = new ArrayList<>();
 
     public IterableSubject(Function propertyFunc, String propertyName) {
         super(IterableSubject.class, propertyFunc, propertyName);
@@ -111,20 +113,32 @@ public class IterableSubject<T> extends Subject<IterableSubject<T>, Iterable<T>>
 
     // isOrdered
 
+    // TODO: the problem with this is that constraint isValid returns a boolean which doesn't work with this
+    // is there a way to turn this into a rule? Problem being that we could define constraints for the actual iterable
+    // as well as the items
+    // Does IterableSubject need to include a collection of item constraints?
+    // Should Subjects contain a rule instead of a list of constraints?
+    public final IterableSubject<T> forEach(Constraint<? super T>... constraintsToAdd) {
+        // TODO: what should we do here? What about a something like CollectionConstraint? CollectionItemConstraint?
+        // fluentValidation has PropertyRule and CollectionPropertyRule so maybe CollectionConstraint doesn't suck too much
+        itemConstraints.addAll(Arrays.asList(constraintsToAdd));
+
+        // constraints.add(new IterableItemConstraint<>(constraintsToAdd));
+        return myself;
+    }
 
     public final IterableSubject<T> forEach(Predicate<? super T> predicate, Constraint<? super T>... constraintsToAdd) {
         // TODO: what should we do here? What about a something like CollectionConstraint? CollectionItemConstraint?
         // fluentValidation has PropertyRule and CollectionPropertyRule so maybe CollectionConstraint doesn't suck too much
-        constraints.add(new CollectionConstraint<>(predicate, constraintsToAdd));
+        for (Constraint<? super T> c : constraintsToAdd) {
+            itemConstraints.add(new SoftConstraint<>(predicate, c));
+        }
+
+        //constraints.add(new IterableItemConstraint<>(predicate, constraintsToAdd));
         return myself;
     }
 
-    public final IterableSubject<T> forEach(Constraint<? super T>... constraintsToAdd) {
-        // TODO: what should we do here? What about a something like CollectionConstraint? CollectionItemConstraint?
-        // fluentValidation has PropertyRule and CollectionPropertyRule so maybe CollectionConstraint doesn't suck too much
-        constraints.add(new CollectionConstraint<>(constraintsToAdd));
-        return myself;
-    }
+    // TODO: should we add a forEach that takes softConstraint?
 
     @Override
     public IterableSubject<T> isNull() {
@@ -140,4 +154,9 @@ public class IterableSubject<T> extends Subject<IterableSubject<T>, Iterable<T>>
     public IterableSubject<T> isEquals(Iterable<T> other) {
         return super.isEquals(other);
     }
+
+    public List<Constraint<? super T>> getItemConstraints() {
+        return itemConstraints;
+    }
+
 }
