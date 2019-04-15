cb080578d38cbe1d3c0210bb8ab604edcf48bdc0
seancarroll <seanc28@gmail.com>
2019-04-15 18:22:06 -0500

    adds RuleSetTests with some empty tests that I still need to implementn

---
+++ jfluentvalidation-core/src/test/java/validators/RuleSetTests.java
@@ -0,0 +1,46 @@
+package validators;
+
+import org.junit.jupiter.api.Test;
+
+class RuleSetTests {
+
+    // RulesetTests
+    // Executes_rules_in_specified_ruleset
+    // Executes_rules_not_specified_in_ruleset
+    // Ruleset_cascades_to_child_validator -- includes validator.RuleFor in ruleset
+    // Ruleset_cascades_to_child_collection_validator -- includes ruleFor items in collection
+    // Executes_multiple_rulesets
+    // Executes_all_rules
+    // Executes_rules_in_default_ruleset_and_specific_ruleset
+    // Trims_spaces
+    // Applies_multiple_rulesets_to_rule
+    // Executes_in_rule_in_ruleset_and_default
+    // Executes_in_rule_in_default_and_none
+
+
+    @Test
+    void unassigned() {
+
+    }
+
+    @Test
+    void unassignedWithDefault() {
+
+    }
+
+    @Test
+    void multipleRuleSet() {
+
+    }
+
+    @Test
+    void multipleRuleSetWithUnassigned() {
+
+    }
+
+    @Test
+    void wildcard() {
+
+    }
+
+}
