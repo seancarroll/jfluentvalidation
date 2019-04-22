21719d2108d83469d9a88920e6d80787cfdd1a94
seancarroll <seanc28@gmail.com>
2019-04-22 06:48:00 -0500

    FileSubject should extend Subject

--- jfluentvalidation-core/src/main/java/jfluentvalidation/core/FileSubject.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/FileSubject.java
@@ -1,5 +1,14 @@
 package jfluentvalidation.core;
 
-public class FileSubject {
+import jfluentvalidation.rules.PropertyRule;
+
+import java.io.File;
+
+public class FileSubject extends Subject<FileSubject, File> {
+
+    public FileSubject(PropertyRule<?, File> rule) {
+        super(FileSubject.class, rule);
+    }
+
 
 }
