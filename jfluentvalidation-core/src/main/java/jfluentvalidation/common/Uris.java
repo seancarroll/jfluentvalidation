5e1109aea783ef767fe10a072b92aa68a852bdad
seancarroll <seanc28@gmail.com>
2019-04-29 18:16:47 -0500

    move pattern out to a private static final variable

--- jfluentvalidation-core/src/main/java/jfluentvalidation/common/Uris.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/common/Uris.java
@@ -9,14 +9,25 @@ import java.util.regex.Pattern;
 
 import static java.util.stream.Collectors.*;
 
+/**
+ *
+ */
 public final class Uris {
+
+    private static final Pattern AMPERSAND_PATTERN = Pattern.compile("&");
 
     private Uris() {
         // statics only
     }
 
+    /**
+     *
+     * @param query
+     * @return
+     */
     public static Map<String, List<String>> getParameters(String query) {
-        return Pattern.compile("&").splitAsStream(query)
+        // TODO: This is probably not the most performant and we should maybe look at making it better
+        return AMPERSAND_PATTERN.splitAsStream(query)
             .map(s -> Arrays.copyOf(s.split("="), 2))
             .collect(groupingBy(s -> decode(s[0]), mapping(s -> decode(s[1]), toList())));
     }
