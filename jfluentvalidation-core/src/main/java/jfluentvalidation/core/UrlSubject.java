6b4931c93b1c2547925c46751226604c16a8675b
seancarroll <seanc28@gmail.com>
2019-04-29 17:15:09 -0500

    add methods to UrlSubject

--- jfluentvalidation-core/src/main/java/jfluentvalidation/core/UrlSubject.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/UrlSubject.java
@@ -1,5 +1,6 @@
 package jfluentvalidation.core;
 
+import jfluentvalidation.constraints.url.*;
 import jfluentvalidation.rules.PropertyRule;
 
 import java.net.URL;
@@ -15,23 +16,83 @@ public class UrlSubject<T> extends Subject<UrlSubject<T>, T, URL> {
     }
 
     public UrlSubject<T> hasProtocol(String expected) {
-        // rule.addConstraint();
+        rule.addConstraint(new HasProtocolConstraint<>(expected));
         return myself;
     }
 
-    // hasPath(string expected)
-    // hasNoPath()
-    // hasPort(int expected)
-    // hasNoPort()
-    // hasHost(string expected)
-    // hasAuthority(string expected)
-    // hasQuery(String expected)
-    // hasNoQuery()
-    // hasAnchor(string expected)
-    // hasNoAnchor
-    // hasParameter(String name)
-    // hasParameter(String name, String value)
-    // hasNoParameters()
-    // hasNoParameter(String name)
-    // hasNoParameter(String name, String value)
+    public UrlSubject<T> hasPath(String expected) {
+        rule.addConstraint(new HasPathConstraint<>(expected));
+        return myself;
+    }
+
+    public UrlSubject<T> hasNoPath(String expected) {
+        rule.addConstraint(new HasPathConstraint<>(""));
+        return myself;
+    }
+
+    public UrlSubject<T> hasPort(int expected) {
+        rule.addConstraint(new HasPortConstraint<>(expected));
+        return myself;
+    }
+
+    public UrlSubject<T> hasNoPort(int expected) {
+        rule.addConstraint(new HasPortConstraint<>(-1));
+        return myself;
+    }
+
+    public UrlSubject<T> hasHost(String expected) {
+        rule.addConstraint(new HasHostConstraint<>(expected));
+        return myself;
+    }
+
+    public UrlSubject<T> hasAuthority(String expected) {
+        rule.addConstraint(new HasAuthorityConstraint<>(expected));
+        return myself;
+    }
+
+    public UrlSubject<T> hasQuery(String expected) {
+        rule.addConstraint(new HasQueryConstraint<>(expected));
+        return myself;
+    }
+
+    public UrlSubject<T> hasNoQuery() {
+        rule.addConstraint(new HasQueryConstraint<>(null));
+        return myself;
+    }
+
+    public UrlSubject<T> hasAnchor(String expected) {
+        rule.addConstraint(new HasAnchorConstraint<>(expected));
+        return myself;
+    }
+
+    public UrlSubject<T> hasNoAnchor() {
+        rule.addConstraint(new HasAnchorConstraint<>(null));
+        return myself;
+    }
+
+    public UrlSubject<T> hasParameter(String name) {
+        rule.addConstraint(new HasParameterConstraint<>(name));
+        return myself;
+    }
+
+    public UrlSubject<T> hasParameter(String name, String value) {
+        rule.addConstraint(new HasParameterConstraint<>(name, value));
+        return myself;
+    }
+
+    public UrlSubject<T> hasNoParameters() {
+        rule.addConstraint(new HasNoParameterConstraint<>());
+        return myself;
+    }
+
+    public UrlSubject<T> hasNoParameters(String name) {
+        rule.addConstraint(new HasNoParameterConstraint<>(name));
+        return myself;
+    }
+
+    public UrlSubject<T> hasNoParameters(String name, String value) {
+        rule.addConstraint(new HasNoParameterConstraint<>(name, value));
+        return myself;
+    }
+
 }
