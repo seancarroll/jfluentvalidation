6d2de7ee48c995419f07b84d409bfe9e0d851518
seancarroll <seanc28@gmail.com>
2019-04-29 21:16:21 -0500

    fix typo

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/ConstraintViolation.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/ConstraintViolation.java
@@ -2,6 +2,6 @@ package jfluentvalidation.constraints;
 
 // TODO: do we like this or ConstraintResult or ValidationFailure or ValidationException
 // I forgot why I might want this?
-// Is there a ConstrationViolation class defined in javax?
+// Is there a ConstraintViolation class defined in javax?
 public class ConstraintViolation {
 }
