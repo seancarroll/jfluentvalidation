5ff6a8196a7a9df779f178eb34116a3d20d51294
seancarroll <seanc28@gmail.com>
2019-04-30 17:46:01 -0500

    implements HasContentConstraint

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/inputstream/HasContentConstraint.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/inputstream/HasContentConstraint.java
@@ -3,7 +3,10 @@ package jfluentvalidation.constraints.inputstream;
 import jfluentvalidation.constraints.Constraint;
 import jfluentvalidation.validators.RuleContext;
 
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -11,9 +14,37 @@ import java.io.InputStream;
  */
 public class HasContentConstraint<T> implements Constraint<T, InputStream> {
 
+    private final String expected;
+
+    public HasContentConstraint(String expected) {
+        this.expected = expected;
+    }
+
     @Override
     public boolean isValid(RuleContext<T, InputStream> context) {
+        try {
+            List<String> actualLines = linesFromBufferedReader(new BufferedReader(new InputStreamReader(context.getPropertyValue(), Charset.defaultCharset())));
+            List<String> expectedExpected = linesFromBufferedReader(new BufferedReader(new StringReader(expected)));
+            // TODO: implement
+            // return expected.equals(context.getPropertyValue().)
+
+        } catch (IOException e) {
+            // String msg = format("Unable to compare contents of InputStream:%n  <%s>%nand String:%n  <%s>", actual, expected);
+            // TODO:
+            // throw new InputStreamsException(msg, e);
+        } finally {
+
+        }
+
         return false;
     }
 
+    private List<String> linesFromBufferedReader(BufferedReader reader) throws IOException {
+        String line;
+        List<String> lines = new ArrayList<>();
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        return lines;
+    }
 }
