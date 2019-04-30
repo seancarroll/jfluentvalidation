208c9058d622bac7541cf231e93803e55293ace4
seancarroll <seanc28@gmail.com>
2019-04-30 17:41:45 -0500

    typo failed to add AbstractComparableNumber

--- jfluentvalidation-core/src/main/java/jfluentvalidation/core/AbstractComparableNumber.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/AbstractComparableNumber.java
@@ -15,77 +15,11 @@ import jfluentvalidation.rules.PropertyRule;
  * @param <A>  the type of the actual object being tested by this {@code Subject}.
  */
 public abstract class AbstractComparableNumber<S extends AbstractComparableNumber<S, T, A>, T, A extends Number & Comparable<A>>
-    extends Subject<S, T, A>
-    implements NumberSubject<S, A>, ComparableSubject<S, T, A> {
+    extends AbstractComparableSubject<S, T, A>
+    implements NumberSubject<S, A> {
 
     public AbstractComparableNumber(Class<?> selfType, PropertyRule<T, A> rule) {
         super(selfType, rule);
-    }
-
-    @Override
-    public S isEqualAccordingToCompareTo(A other) {
-        rule.addConstraint(new IsEqualAccordingToCompareToConstraint<>(other));
-        return myself;
-    }
-
-    @Override
-    public S isNotEqualAccordingToCompareTo(A other) {
-        rule.addConstraint(new IsNotEqualAccordingToCompareToConstraint<>(other));
-        return myself;
-    }
-
-    @Override
-    public S isLessThan(A other) {
-        rule.addConstraint(new IsLessThanConstraint<>(other));
-        return myself;
-    }
-
-    @Override
-    public S isLessThanOrEqualTo(A other) {
-        rule.addConstraint(new IsLessThanOrEqualToConstraint<>(other));
-        return myself;
-    }
-
-    @Override
-    public S isGreaterThan(A other) {
-        rule.addConstraint(new IsGreaterThanConstraint<>(other));
-        return myself;
-    }
-
-    @Override
-    public S isGreaterThanOrEqualTo(A other) {
-        rule.addConstraint(new IsGreaterThanOrEqualToConstraint<>(other));
-        return myself;
-    }
-
-    @Override
-    public S isBetween(A startInclusive, A endInclusive) {
-        rule.addConstraint(new IsBetweenConstraint<>(startInclusive, endInclusive, true, true));
-        return myself;
-    }
-
-    @Override
-    public S isStrictlyBetween(A startExclusive, A endExclusive) {
-        rule.addConstraint(new IsBetweenConstraint<>(startExclusive, endExclusive, true, true));
-        return myself;
-    }
-
-    @Override
-    public S isBetween(A start, A end, boolean inclusiveStart, boolean inclusiveEnd) {
-        rule.addConstraint(new IsBetweenConstraint<>(start, end, inclusiveStart, inclusiveEnd));
-        return myself;
-    }
-
-    public S isNotBetwen(A start, A end) {
-        // TODO: should it be inclusive start and end?
-        rule.addConstraint(new IsNotBetweenConstraint<>(start, end, true, true));
-        return myself;
-    }
-
-    public S isNotBetwen(A start, A end, boolean inclusiveStart, boolean inclusiveEnd) {
-        // TODO: should it be inclusive start and end?
-        rule.addConstraint(new IsNotBetweenConstraint(start, end, inclusiveStart, inclusiveEnd));
-        return myself;
     }
 
     @Override
@@ -145,16 +79,6 @@ public abstract class AbstractComparableNumber<S extends AbstractComparableNumbe
 
     @Override
     public S isNotCloseTo() {
-        return null;
-    }
-
-    @Override
-    public S isNotBetween(A startInclusive, A endInclusive) {
-        return null;
-    }
-
-    @Override
-    public S isNotBetween(A start, A end, boolean inclusiveStart, boolean inclusiveEnd) {
         return null;
     }
 
