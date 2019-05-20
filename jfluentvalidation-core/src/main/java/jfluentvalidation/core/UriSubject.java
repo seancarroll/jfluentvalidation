df59fa6198b826c39036f68917ea16f63a7550af
seancarroll <seanc28@gmail.com>
2019-05-19 21:52:34 -0500

    improve javadoc

--- jfluentvalidation-core/src/main/java/jfluentvalidation/core/UriSubject.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/UriSubject.java
@@ -8,7 +8,8 @@ import java.net.URI;
 
 /**
  *
- * @param <T>  the type of the instance
+ * @param <T>  the type of the instance.
+ * @see java.net.URI
  */
 public class UriSubject<T> extends Subject<UriSubject<T>, T, URI> implements ComparableSubject<UriSubject<T>, T, URI> {
 
@@ -16,66 +17,142 @@ public class UriSubject<T> extends Subject<UriSubject<T>, T, URI> implements Com
         super(UriSubject.class, rule);
     }
 
+    /**
+     * Verifies that the actual {@code URI} has the expected path.
+     *
+     * @param expected
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasPath(String expected) {
         rule.addConstraint(new HasPathConstraint<>(expected));
         return myself;
     }
 
-    public UriSubject<T> hasNoPath(String expected) {
+    /**
+     * Verifies that the actual {@code URI} has no path.
+     *
+     * @return {@code this} Uri subject.
+     */
+    public UriSubject<T> hasNoPath() {
         rule.addConstraint(new HasPathConstraint<>(""));
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} has the expected port.
+     *
+     * @param expected
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasPort(int expected) {
         rule.addConstraint(new HasPortConstraint<>(expected));
         return myself;
     }
 
-    public UriSubject<T> hasNoPort(int expected) {
+    /**
+     * Verifies that the actual {@code URI} has no port.
+     *
+     * @return {@code this} Uri subject.
+     */
+    public UriSubject<T> hasNoPort() {
         rule.addConstraint(new HasPortConstraint<>(-1));
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} has the expected host.
+     *
+     * @param expected  the expected host of the actual {@code URI}.
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasHost(String expected) {
         rule.addConstraint(new HasHostConstraint<>(expected));
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} has the expected authority.
+     *
+     * @param expected  the expected authority of the actual {@code URI}.
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasAuthority(String expected) {
         rule.addConstraint(new HasAuthorityConstraint<>(expected));
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} has the expected query.
+     *
+     * @param expected  the expected query of the actual {@code URI}.
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasQuery(String expected) {
         rule.addConstraint(new HasQueryConstraint<>(expected));
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} has no query.
+     *
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasNoQuery() {
         rule.addConstraint(new HasQueryConstraint<>(null));
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} has a parameter with the expected name.
+     *
+     * @param name  the name of the parameter expected to be present.
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasParameter(String name) {
         rule.addConstraint(new HasParameterConstraint<>(name));
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} has a parameter with the expected name and value.
+     *
+     * @param name  the name of the parameter expected to be present.
+     * @param value  the value of the parameter expected to be present.
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasParameter(String name, String value) {
         rule.addConstraint(new HasParameterConstraint<>(name, value));
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} does not have any parameters.
+     *
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasNoParameters() {
         rule.addConstraint(new HasNoParameterConstraint<>());
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} does not have a parameter with the specified name.
+     *
+     * @param name  the name of the parameter expected to be absent.
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasNoParameters(String name) {
         rule.addConstraint(new HasNoParameterConstraint<>(name));
         return myself;
     }
 
+    /**
+     * Verifies that the actual {@code URI} does not have a parameter with the expected name and value.
+     *
+     * @param name  the name of the parameter expected to be absent.
+     * @param value  the value of the parameter expected to be absent.
+     * @return {@code this} Uri subject.
+     */
     public UriSubject<T> hasNoParameters(String name, String value) {
         rule.addConstraint(new HasNoParameterConstraint<>(name, value));
         return myself;
--- jfluentvalidation-core/src/main/java/jfluentvalidation/core/UrlSubject.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/UrlSubject.java
@@ -7,7 +7,8 @@ import java.net.URL;
 
 /**
  *
- * @param <T>  the type of the instance
+ * @param <T>  the type of the instance.
+ * @see java.net.URL
  */
 public class UrlSubject<T> extends Subject<UrlSubject<T>, T, URL> {
 
@@ -15,81 +16,158 @@ public class UrlSubject<T> extends Subject<UrlSubject<T>, T, URL> {
         super(UrlSubject.class, rule);
     }
 
+    /**
+     *
+     * @param expected
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasProtocol(String expected) {
         rule.addConstraint(new HasProtocolConstraint<>(expected));
         return myself;
     }
 
+    /**
+     *
+     * @param expected  the expected path of the actual {@code URL}.
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasPath(String expected) {
         rule.addConstraint(new HasPathConstraint<>(expected));
         return myself;
     }
 
-    public UrlSubject<T> hasNoPath(String expected) {
+    /**
+     *
+     * @return {@code this} Url subject.
+     */
+    public UrlSubject<T> hasNoPath() {
         rule.addConstraint(new HasPathConstraint<>(""));
         return myself;
     }
 
+    /**
+     *
+     * @param expected
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasPort(int expected) {
         rule.addConstraint(new HasPortConstraint<>(expected));
         return myself;
     }
 
-    public UrlSubject<T> hasNoPort(int expected) {
+    /**
+     * Verifies that the actual {@code URI} has no port.
+     * @return {@code this} Url subject.
+     */
+    public UrlSubject<T> hasNoPort() {
         rule.addConstraint(new HasPortConstraint<>(-1));
         return myself;
     }
 
+    /**
+     *
+     * @param expected
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasHost(String expected) {
         rule.addConstraint(new HasHostConstraint<>(expected));
         return myself;
     }
 
+    /**
+     *
+     * @param expected
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasAuthority(String expected) {
         rule.addConstraint(new HasAuthorityConstraint<>(expected));
         return myself;
     }
 
+    /**
+     *
+     * @param expected
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasQuery(String expected) {
         rule.addConstraint(new HasQueryConstraint<>(expected));
         return myself;
     }
 
+    /**
+     *
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasNoQuery() {
         rule.addConstraint(new HasQueryConstraint<>(null));
         return myself;
     }
 
+    /**
+     *
+     * @param expected
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasAnchor(String expected) {
         rule.addConstraint(new HasAnchorConstraint<>(expected));
         return myself;
     }
 
+    /**
+     *
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasNoAnchor() {
         rule.addConstraint(new HasAnchorConstraint<>(null));
         return myself;
     }
 
+    /**
+     *
+     * @param name
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasParameter(String name) {
         rule.addConstraint(new HasParameterConstraint<>(name));
         return myself;
     }
 
+    /**
+     *
+     * @param name
+     * @param value
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasParameter(String name, String value) {
         rule.addConstraint(new HasParameterConstraint<>(name, value));
         return myself;
     }
 
+    /**
+     *
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasNoParameters() {
         rule.addConstraint(new HasNoParameterConstraint<>());
         return myself;
     }
 
+    /**
+     *
+     * @param name
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasNoParameters(String name) {
         rule.addConstraint(new HasNoParameterConstraint<>(name));
         return myself;
     }
 
+    /**
+     *
+     * @param name
+     * @param value
+     * @return {@code this} Url subject.
+     */
     public UrlSubject<T> hasNoParameters(String name, String value) {
         rule.addConstraint(new HasNoParameterConstraint<>(name, value));
         return myself;
