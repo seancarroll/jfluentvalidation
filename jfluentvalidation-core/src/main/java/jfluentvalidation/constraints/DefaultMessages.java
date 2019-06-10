e45bfb13907534f32d92dd5f004e93570372d835
seancarroll <seanc28@gmail.com>
2019-06-10 06:33:59 -0500

    adds DefaultMessages which contains static strings which will eventually be used as part of some localization logic

---
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/DefaultMessages.java
@@ -0,0 +1,120 @@
+package jfluentvalidation.constraints;
+
+public class DefaultMessages {
+
+    public static final String IS_EMPTY = "{jfluentvalidation.constraints.IsEmpty.message}";
+    public static final String IS_EQUALS = "{jfluentvalidation.constraints.IsEquals.message}";
+    public static final String IS_NOT_EMPTY = "{jfluentvalidation.constraints.IsNotEmpty.message}";
+    public static final String IS_NOT_EQUALS = "{jfluentvalidation.constraints.IsNotEquals.message}";
+    public static final String IS_NOT_NULL = "{jfluentvalidation.constraints.IsNotNUll.message}";
+    public static final String IS_NULL = "{jfluentvalidation.constraints.IsNull.message}";
+
+    public static final String HAS_SIZE = "{jfluentvalidation.constraints.HasSize.message}";
+
+    // TODO: is this the same as other contain
+    public static final String ARRAY_CONTAINS = "{jfluentvalidation.constraints.array.Contains.message}";
+    public static final String ARRAY_EXACT_LENGTH = "{jfluentvalidation.constraints.array.length.ArrayExactLength.message}";
+    public static final String ARRAY_LENGTH = "{jfluentvalidation.constraints.array.length.ArrayLength.message}";
+    public static final String ARRAY_BETWEEN_LENGTH = "{jfluentvalidation.constraints.array.length.ArrayBetweenLength.message}";
+    public static final String ARRAY_MAXIMUM_LENGTH = "{jfluentvalidation.constraints.array.length.ArrayMaximumLength.message}";
+    public static final String ARRAY_MINIMUM_LENGTH = "{jfluentvalidation.constraints.array.length.BooleanArrayMinimumLength.message}";
+
+
+    // TODO: is this the same as other contains?
+    public static final String CHARSEQUENCE_CONTAINS = "{jfluentvalidation.constraints.charsequence.Contains.message}";
+    public static final String CHARSEQUENCE_CONTAINS_IGNORE_CASE = "{jfluentvalidation.constraints.charsequence.ContainsIgnoreCase.message}";
+    public static final String CHARSEQUENCE_CONTAINS_ONLY_DIGITS = "{jfluentvalidation.constraints.charsequence.ContainsOnlyDigits.message}";
+    public static final String CHARSEQUENCE_CONTAINS_ONLY_ONCE = "{jfluentvalidation.constraints.charsequence.ContainsOnlyOnce.message}";
+    public static final String CHARSEQUENCE_CONTAINS_ONLY_WHITESPACES = "{jfluentvalidation.constraints.charsequence.ContainsOnlyWhitespaces.message}";
+    public static final String CHARSEQUENCE_CONTAINS_PATTERN = "{jfluentvalidation.constraints.charsequence.ContainsPattern.message}";
+    public static final String CHARSEQUENCE_CONTAINS_SEQUENCE = "{jfluentvalidation.constraints.charsequence.ContainsSequence.message}";
+    public static final String CHARSEQUENCE_CONTAINS_SUBSEQUENCE = "{jfluentvalidation.constraints.charsequence.ContainsSubsequence.message}";
+    public static final String CHARSEQUENCE_CONTAINS_WHITESPACES = "{jfluentvalidation.constraints.charsequence.ContainsIgnoreCase.message}";
+    public static final String CHARSEQUENCE_DOES_NOT_CONTAIN_ANY_WHITESPACES = "{jfluentvalidation.constraints.charsequence.DoesNotContainAnyWhitespaces.message}";
+    public static final String CHARSEQUENCE_ENDS_WITH = "{jfluentvalidation.constraints.charsequence.EndsWith.message}";
+    public static final String CHARSEQUENCE_HAS_LENGTH_BETWEEN = "{jfluentvalidation.constraints.charsequence.HasLengthBetween.message}";
+    public static final String CHARSEQUENCE_HAS_LENGTH = "{jfluentvalidation.constraints.charsequence.HasLength.message}";
+    public static final String CHARSEQUENCE_HAS_LENGTH_GREATER_THAN = "{jfluentvalidation.constraints.charsequence.HasLengthGreaterThan.message}";
+    public static final String CHARSEQUENCE_HAS_LENGTH_GREATER_THAN_OR_EQUAL = "{jfluentvalidation.constraints.charsequence.HasLengthGreaterThanOrEqualTo.message}";
+    public static final String CHARSEQUENCE_HAS_LENGTH_LESS_THAN = "{jfluentvalidation.constraints.charsequence.HasLengthLessThan.message}";
+    public static final String CHARSEQUENCE_HAS_LENGTH_LESS_THAN_OR_EQUAL = "{jfluentvalidation.constraints.charsequence.HasLengthLessThanOrEqualTo.message}";
+    public static final String CHARSEQUENCE_HAS_SAME_LENGTH_AS = "{jfluentvalidation.constraints.charsequence.HasSameLengthAs.message}";
+    public static final String CHARSEQUENCE_IS_BLANK = "{jfluentvalidation.constraints.charsequence.IsBlank.message}";
+    public static final String CHARSEQUENCE_IS_EMAIL = "{jfluentvalidation.constraints.charsequence.IsEmail.message}";
+    public static final String CHARSEQUENCE_IS_EQUAL_TO_IGNORING_CASE = "{jfluentvalidation.constraints.charsequence.IsEqualToIgnoringCase.message}";
+    public static final String CHARSEQUENCE_IS_EQUAL_TO_IGNORING_WHITESPACE = "{jfluentvalidation.constraints.charsequence.IsEqualToIgnoringWhitespace.message}";
+    public static final String CHARSEQUENCE_IS_EQUAL_TO_NORMALIZING_WHITESPACE = "{jfluentvalidation.constraints.charsequence.IsEqualToNormalizingWhitespace.message}";
+    public static final String CHARSEQUENCE_IS_LOWER_CASE = "{jfluentvalidation.constraints.charsequence.IsLowerCase.message}";
+    public static final String CHARSEQUENCE_IS_SUBSTRING_OF = "{jfluentvalidation.constraints.charsequence.IsSubstringOf.message}";
+    public static final String CHARSEQUENCE_IS_UPPER_CASE = "{jfluentvalidation.constraints.charsequence.IsUppserCase.message}";
+    public static final String CHARSEQUENCE_LENGTH = "{jfluentvalidation.constraints.charsequence.Length.message}";
+    public static final String CHARSEQUENCE_MATCHES = "{jfluentvalidation.constraints.charsequence.MatchesConstraint.message}";
+    public static final String CHARSEQUENCE_STARTS_WITH = "{jfluentvalidation.constraints.charsequence.StartsWith.message}";
+
+
+
+    public static final String COMPARABLE_IS_BETWEEN = "{jfluentvalidation.constraints.comparable.IsBetween.message}";
+    public static final String COMPARABLE_IS_EQUAL_ACCORDING_TO_COMPARE_TO = "{jfluentvalidation.constraints.comparable.IsEqualAccordingToCompareTo.message}";
+    public static final String COMPARABLE_IS_GREATER_THAN = "{jfluentvalidation.constraints.comparable.IsGreaterThan.message}";
+    public static final String COMPARABLE_IS_GREATER_THAN_OR_EQUAL_TO = "{jfluentvalidation.constraints.comparable.IsGreaterThanOrEqualTo.message}";
+    public static final String COMPARABLE_IS_LESS_THAN = "{jfluentvalidation.constraints.comparable.IsLessThan.message}";
+    public static final String COMPARABLE_IS_LESS_THAN_OR_EQUAL_TO = "{jfluentvalidation.constraints.comparable.IsLessThanOrEqualTo.message}";
+    public static final String COMPARABLE_IS_NOT_BETWEEN = "{jfluentvalidation.constraints.comparable.IsNotBetween.message}";
+    public static final String COMPARABLE_IS_NOT_EQUAL_ACCORDING_TO_COMPARE_TO = "{jfluentvalidation.constraints.comparable.IsNotEqualAccordingToCompareTo.message}";
+
+
+    public static final String FILE_CAN_READ = "{jfluentvalidation.constraints.file.CanRead.message}";
+    public static final String FILE_CAN_WRITE = "{jfluentvalidation.constraints.file.CanWrite.message}";
+    public static final String FILE_EXISTS = "{jfluentvalidation.constraints.file.FileExists.message}";
+    // TODO: is this the same as inputstream_has_content?
+    public static final String FILE_HAS_CONTENT = "{jfluentvalidation.constraints.file.HasContent.message}";
+    public static final String FILE_HAS_EXTENSION = "{jfluentvalidation.constraints.file.HasExtension.message}";
+    public static final String FILE_HAS_NAME = "{jfluentvalidation.constraints.file.HasName.message}";
+    public static final String FILE_IS_ABSOLUTE = "{jfluentvalidation.constraints.file.IsAbsolute.message}";
+    public static final String FILE_IS_DIRECTORY = "{jfluentvalidation.constraints.file.IsDirectory.message}";
+    public static final String FILE_IS_FILE = "{jfluentvalidation.constraints.file.IsFile.message}";
+    public static final String FILE_IS_RELATIVE = "{jfluentvalidation.constraints.file.IsRelative.message}";
+
+
+    public static final String INPUTSTREAM_HAS_CONTENT = "{jfluentvalidation.constraints.inputstream.HasContent.message}";
+
+
+    public static final String ITERABLE_CONTAINS_ALL_IN = "{jfluentvalidation.constraints.iterable.ContainsAllIn.message}";
+    public static final String ITERABLE_CONTAINS_ANY_IN = "{jfluentvalidation.constraints.iterable.ContainsAnyIn.message}";
+    public static final String ITERABLE_CONTAINS = "{jfluentvalidation.constraints.iterable.Contains.message}";
+    public static final String ITERABLE_CONTAINS_EXACTLY_ELEMENTS_IN = "{jfluentvalidation.constraints.iterable.ContainsExactlyElementsIn.message}";
+    public static final String ITERABLE_CONTAINS_NONE_IN = "{jfluentvalidation.constraints.iterable.ContainsNoneIn.message}";
+    public static final String ITERABLE_DOES_NOT_CONTAIN = "{jfluentvalidation.constraints.iterable.DoesNotContain.message}";
+    public static final String ITERABLE_ITEM = "{jfluentvalidation.constraints.iterable.Item.message}";
+    // TODO: this name sucks
+    public static final String ITERABLE_ITERABLE_TEM = "{jfluentvalidation.constraints.iterable.IterableItem.message}";
+
+
+    public static final String MAP_CONTAINS_ENTRY = "{jfluentvalidation.constraints.map.ContainsEntry.message}";
+    public static final String MAP_CONTAINS_KEY = "{jfluentvalidation.constraints.map.ContainsKey.message}";
+    public static final String MAP_CONTAINS_VALUE = "{jfluentvalidation.constraints.map.ContainsValue.message}";
+    public static final String MAP_ENTRY = "{jfluentvalidation.constraints.map.Entry.message}";
+    public static final String MAP_KEY = "{jfluentvalidation.constraints.map.Key.message}";
+    public static final String MAP_VALUE = "{jfluentvalidation.constraints.map.Value.message}";
+
+
+    public static final String TIME_IS_AFTER = "{jfluentvalidation.constraints.time.IsAfter.message}";
+    public static final String TIME_IS_AFTER_OR_EQUAL = "{jfluentvalidation.constraints.time.IsAfterOrEqual.message}";
+    public static final String TIME_IS_BEFORE = "{jfluentvalidation.constraints.time.IsBefore.message}";
+    public static final String TIME_IS_BEFORE_OR_EQUAL = "{jfluentvalidation.constraints.time.IsBeforeOrEqual.message}";
+
+    public static final String HAS_AUTHORITY = "{jfluentvalidation.constraints.HasAuthority.message}";
+    public static final String HAS_HOST = "{jfluentvalidation.constraints.HasHost.message}";
+    public static final String HAS_NO_PARAMETER = "{jfluentvalidation.constraints.HasNoParameter.message}";
+    public static final String HAS_PARAMETER = "{jfluentvalidation.constraints.HasParameter.message}";
+    public static final String HAS_PATH = "{jfluentvalidation.constraints.HasPath.message}";
+    public static final String HAS_PORT = "{jfluentvalidation.constraints.HasPort.message}";
+    public static final String HAS_QUERY = "{jfluentvalidation.constraints.HasQuery.message}";
+
+    public static final String URL_HAS_ANCHOR = "{jfluentvalidation.constraints.url.HasAnchor.message}";
+    public static final String URL_HAS_PROTOCOL = "{jfluentvalidation.constraints.url.HasProtocol.message}";
+
+    private DefaultMessages() {
+        // public statics only
+    }
+}
