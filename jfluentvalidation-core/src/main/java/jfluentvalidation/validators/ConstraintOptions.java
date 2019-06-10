8a0aef1a5fa254d6a88e58107e02e1558658b4a4
seancarroll <seanc28@gmail.com>
2019-06-10 06:35:35 -0500

    addsConstraintOption which will allow us to override constraint error message with withMessage

---
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/validators/ConstraintOptions.java
@@ -0,0 +1,15 @@
+package jfluentvalidation.validators;
+
+// FluentValidation ValidatorMetadata / PropertyValidatorOptions
+public class ConstraintOptions {
+
+    private String errorMessage;
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
