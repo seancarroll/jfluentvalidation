fd9092fec934d0ee6a74945735eff7396eebdff2
seancarroll <seanc28@gmail.com>
2019-06-16 22:22:18 -0500

    adds isRelative tests

---
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/file/IsRelativeFileTest.java
@@ -0,0 +1,61 @@
+package jfluentvalidation.validators.rulefor.file;
+
+import jfluentvalidation.ValidationFailure;
+import jfluentvalidation.validators.DefaultValidator;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class IsRelativeFileTest {
+
+    @Test
+    void shouldReturnFailureWhenActualIsNull() {
+        Target t = new Target(null);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFile(Target::getFile).isRelative();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+
+    @Test
+    void shouldNotReturnFailureWhenFileIsRelative() {
+        File actual = mock(File.class);
+        when(actual.isAbsolute()).thenReturn(false);
+
+        Target t = new Target(actual);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFile(Target::getFile).isRelative();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+
+    @Test
+    void shouldReturnFailureWhenFileIsNotRelative() {
+        File actual = mock(File.class);
+        when(actual.isAbsolute()).thenReturn(true);
+
+        Target t = new Target(actual);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFile(Target::getFile).isRelative();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+}
