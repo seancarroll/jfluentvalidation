7cdb70405240dd031651d1d5882541383dcf5efb
seancarroll <seanc28@gmail.com>
2019-06-16 20:49:21 -0500

    adds methods to FileSubject

--- jfluentvalidation-core/src/main/java/jfluentvalidation/core/FileSubject.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/FileSubject.java
@@ -1,9 +1,11 @@
 package jfluentvalidation.core;
 
 import jfluentvalidation.constraints.comparable.*;
+import jfluentvalidation.constraints.file.*;
 import jfluentvalidation.rules.PropertyRule;
 
 import java.io.File;
+import java.nio.charset.Charset;
 
 /**
  *
@@ -14,6 +16,65 @@ public class FileSubject<T> extends Subject<FileSubject<T>, T, File> implements 
 
     public FileSubject(PropertyRule<T, File> rule) {
         super(FileSubject.class, rule);
+    }
+
+    // TODO: add the following?
+    // hasSameContentAs(File expected)
+    // hasSameContentAs(File expected, Charset expectedCharset)
+
+
+    public FileSubject<T> canRead() {
+        rule.addConstraint(new CanReadConstraint<>());
+        return myself;
+    }
+
+    public FileSubject<T> canWrite() {
+        rule.addConstraint(new CanWriteConstraint<>());
+        return myself;
+    }
+
+    public FileSubject<T> exists() {
+        rule.addConstraint(new FileExistsConstraint<>());
+        return myself;
+    }
+
+    public FileSubject<T> hasContent(String content) {
+        return hasContent(content, Charset.defaultCharset());
+    }
+
+    public FileSubject<T> hasContent(String content, Charset charset) {
+        rule.addConstraint(new HasContentConstraint<>(content, charset));
+        return myself;
+    }
+
+    public FileSubject<T> hasExtension(String extension) {
+        rule.addConstraint(new HasExtensionConstraint<>(extension));
+        return myself;
+    }
+
+    public FileSubject<T> hasName(String name) {
+        rule.addConstraint(new HasNameConstraint<>(name));
+        return myself;
+    }
+
+    public FileSubject<T> isAbsolute() {
+        rule.addConstraint(new IsAbsoluteConstraint<>());
+        return myself;
+    }
+
+    public FileSubject<T> isDirectory() {
+        rule.addConstraint(new IsDirectoryConstraint<>());
+        return myself;
+    }
+
+    public FileSubject<T> isFile() {
+        rule.addConstraint(new IsFileConstraint<>());
+        return myself;
+    }
+
+    public FileSubject<T> isRelative() {
+        rule.addConstraint(new IsRelativeConstraint<>());
+        return myself;
     }
 
     @Override
