f8cc3580b4f36e04844bd06ffeaf5a84c751029d
seancarroll <seanc28@gmail.com>
2019-06-23 16:12:29 -0500

    adds additional ruleForFloat tests

---
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/floats/IsNotPositiveTest.java
@@ -0,0 +1,62 @@
+package jfluentvalidation.validators.rulefor.floats;
+
+import jfluentvalidation.ValidationFailure;
+import jfluentvalidation.validators.DefaultValidator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class IsNotPositiveTest {
+
+    @Test
+    void shouldNotReturnFailureWhenActualIsNotPositive() {
+        Target t = new Target(-1f);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFloat(Target::getNumber).isNotPositive();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+    @Test
+    void shouldNotReturnFailureWhenActualIsZero() {
+        Target t = new Target(0f);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFloat(Target::getNumber).isNotPositive();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsPositive() {
+        Target t = new Target(1f);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFloat(Target::getNumber).isNotPositive();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsNull() {
+        Target t = new Target(null);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFloat(Target::getNumber).isNotPositive();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+}
