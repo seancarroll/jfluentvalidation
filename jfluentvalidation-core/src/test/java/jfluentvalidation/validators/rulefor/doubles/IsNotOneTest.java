e288be3c8ebe5bdbfdbfc5f2ea05b221782bbf07
seancarroll <seanc28@gmail.com>
2019-06-23 16:09:12 -0500

    adds additional ruleForDouble tests

---
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/doubles/IsNotOneTest.java
@@ -0,0 +1,50 @@
+package jfluentvalidation.validators.rulefor.floats;
+
+import jfluentvalidation.ValidationFailure;
+import jfluentvalidation.validators.DefaultValidator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class IsNotOneTest {
+
+    @Test
+    void shouldNotReturnFailureWhenActualIsNotOne() {
+        Target t = new Target(2f);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFloat(Target::getNumber).isNotOne();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsOne() {
+        Target t = new Target(1f);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFloat(Target::getNumber).isNotOne();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsNull() {
+        Target t = new Target(null);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForFloat(Target::getNumber).isNotOne();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+}
