32fc20daa22841768260ba5a8fdab93085dc08b4
seancarroll <seanc28@gmail.com>
2019-06-26 20:21:33 -0500

    adds additional ruleForZonedDateTime tests

--- jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/zoneddatetime/IsInTheFutureOrPresent.java
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/zoneddatetime/IsInTheFutureOrPresent.java
@@ -1,4 +1,55 @@
 package jfluentvalidation.validators.rulefor.zoneddatetime;
 
+import jfluentvalidation.ValidationFailure;
+import jfluentvalidation.validators.DefaultValidator;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class IsInTheFutureOrPresent {
+
+    private static final ZonedDateTime PAST = ZonedDateTime.now().minusDays(1);
+    private static final ZonedDateTime FUTURE = ZonedDateTime.now().plusDays(1);
+
+    // TODO: test for same zonedDateTime. need to implement clock.
+
+    @Test
+    void shouldNotReturnFailureWhenActualIsInTheFuture() {
+        Target t = new Target(FUTURE);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForZonedDateTime(Target::getDateTime).isInTheFutureOrPresent();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsInThePast() {
+        Target t = new Target(PAST);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForZonedDateTime(Target::getDateTime).isInTheFutureOrPresent();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsNull() {
+        Target t = new Target(null);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForZonedDateTime(Target::getDateTime).isInTheFutureOrPresent();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
 }
--- jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/zoneddatetime/IsInThePastOrPresentTest.java
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/zoneddatetime/IsInThePastOrPresentTest.java
@@ -1,4 +1,55 @@
 package jfluentvalidation.validators.rulefor.zoneddatetime;
 
+import jfluentvalidation.ValidationFailure;
+import jfluentvalidation.validators.DefaultValidator;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class IsInThePastOrPresentTest {
+
+    private static final ZonedDateTime PAST = ZonedDateTime.now().minusDays(1);
+    private static final ZonedDateTime FUTURE = ZonedDateTime.now().plusDays(1);
+
+    // TODO: test for same zonedDateTime. need to implement clock.
+
+    @Test
+    void shouldNotReturnFailureWhenActualIsInThePast() {
+        Target t = new Target(PAST);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForZonedDateTime(Target::getDateTime).isInThePastOrPresent();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsInTheFuture() {
+        Target t = new Target(FUTURE);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForZonedDateTime(Target::getDateTime).isInThePastOrPresent();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsNull() {
+        Target t = new Target(null);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForZonedDateTime(Target::getDateTime).isInThePastOrPresent();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
 }
