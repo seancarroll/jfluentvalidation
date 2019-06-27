0cd53c8bd962dd7851f6e49dbba747fc961051cd
seancarroll <seanc28@gmail.com>
2019-06-26 20:22:13 -0500

    adds additional ruleForLocalTime tests

---
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/localtime/IsInThePastOrPresentTest.java
@@ -0,0 +1,55 @@
+package jfluentvalidation.validators.rulefor.localtime;
+
+import jfluentvalidation.ValidationFailure;
+import jfluentvalidation.validators.DefaultValidator;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class IsInThePastOrPresentTest {
+
+    private static final LocalTime PAST = LocalTime.now().minusHours(1);
+    private static final LocalTime FUTURE = LocalTime.now().plusHours(1);
+
+    // TODO: test for same zonedDateTime. need to implement clock.
+
+    @Test
+    void shouldNotReturnFailureWhenActualIsInThePast() {
+        Target t = new Target(PAST);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForLocalTime(Target::getTime).isInThePastOrPresent();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsInTheFuture() {
+        Target t = new Target(FUTURE);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForLocalTime(Target::getTime).isInThePastOrPresent();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenActualIsNull() {
+        Target t = new Target(null);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForLocalTime(Target::getTime).isInThePastOrPresent();
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+}
