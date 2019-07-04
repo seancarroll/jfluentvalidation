9e0f83f996b90bf667e4c3e6df840ac227cf7f47
seancarroll <seanc28@gmail.com>
2019-07-03 21:55:14 -0500

    adds NegateConstraint. not currently using it and we might not even need it but committing it anyway

---
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/NegateConstraint.java
@@ -0,0 +1,22 @@
+package jfluentvalidation.constraints;
+
+import jfluentvalidation.validators.RuleContext;
+
+// TODO: not sure how to handle error messages.
+// this might not be a good idea and we might not even need it.
+// we could make it abstract
+public abstract class NegateConstraint<T, P> extends AbstractConstraint<T, P> {
+
+    private final Constraint<T, P> innerConstraint;
+
+    public NegateConstraint(Constraint<T, P> innerConstraint) {
+        // TODO: if we did this what would the error message be?
+        super(innerConstraint.getOptions().getErrorMessage());
+        this.innerConstraint = innerConstraint;
+    }
+
+    @Override
+    public boolean isValid(RuleContext<T, P> context) {
+        return !innerConstraint.isValid(context);
+    }
+}
