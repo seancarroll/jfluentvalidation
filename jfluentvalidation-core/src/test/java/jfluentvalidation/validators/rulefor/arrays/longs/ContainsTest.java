efeba7aebe0d104b18857486a0e392bf64d3d961
seancarroll <seanc28@gmail.com>
2019-07-16 21:39:12 -0500

    how could I forget about longs

---
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/arrays/longs/ContainsTest.java
@@ -0,0 +1,25 @@
+package jfluentvalidation.validators.rulefor.arrays.longs;
+
+import jfluentvalidation.ValidationFailure;
+import jfluentvalidation.validators.DefaultValidator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ContainsTest {
+
+    @Test
+    void shouldNotReturnFailureWhenActualContainsGivenValue() {
+        Target t = new Target(new long[] {1});
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForLongArray(Target::getValue).contains(1L);
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+}
---
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/arrays/longs/Target.java
@@ -0,0 +1,13 @@
+package jfluentvalidation.validators.rulefor.arrays.longs;
+
+class Target {
+    private final long[] value;
+
+    public Target(long[] value) {
+        this.value = value;
+    }
+
+    public long[] getValue() {
+        return value;
+    }
+}
