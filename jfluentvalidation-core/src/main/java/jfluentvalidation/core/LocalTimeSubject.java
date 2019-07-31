1ae7e998ccbd278f17d2cbc7d9ba212412faba65
seancarroll <seanc28@gmail.com>
2019-07-31 08:00:54 -0500

    adds @CanIgnoreReturnValue to LocalTimeSubject methods

--- jfluentvalidation-core/src/main/java/jfluentvalidation/core/LocalTimeSubject.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/LocalTimeSubject.java
@@ -1,5 +1,6 @@
 package jfluentvalidation.core;
 
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import jfluentvalidation.constraints.time.IsAfterLocalTimeConstraint;
 import jfluentvalidation.constraints.time.IsAfterOrEqualLocalTimeConstraint;
 import jfluentvalidation.constraints.time.IsBeforeLocalTimeConstraint;
@@ -23,41 +24,49 @@ public class LocalTimeSubject<T>
 
     // QUESTION: which do we want to keep? isBefore / isAfter vs past / future?
     // before and after use compareTo...do we want to keep both?
+    @CanIgnoreReturnValue
     public LocalTimeSubject<T> isBefore(LocalTime other) {
         rule.addConstraint(new IsBeforeLocalTimeConstraint<>(other));
         return  myself;
     }
 
+    @CanIgnoreReturnValue
     public LocalTimeSubject<T> isBeforeOrEqualTo(LocalTime other) {
         rule.addConstraint(new IsBeforeOrEqualLocalTimeConstraint<>(other));
         return  myself;
     }
 
+    @CanIgnoreReturnValue
     public LocalTimeSubject<T> isAfter(LocalTime other) {
         rule.addConstraint(new IsAfterLocalTimeConstraint<>(other));
         return  myself;
     }
 
+    @CanIgnoreReturnValue
     public LocalTimeSubject<T> isAfterOrEqualTo(LocalTime other) {
         rule.addConstraint(new IsAfterOrEqualLocalTimeConstraint<>(other));
         return  myself;
     }
 
+    @CanIgnoreReturnValue
     public LocalTimeSubject<T> isInTheFuture() {
         // TODO: clock from context/provider
         return isAfter(LocalTime.now());
     }
 
+    @CanIgnoreReturnValue
     public LocalTimeSubject<T> isInTheFutureOrPresent() {
         // TODO: clock from context/provider
         return isAfterOrEqualTo(LocalTime.now());
     }
 
+    @CanIgnoreReturnValue
     public LocalTimeSubject<T> isInThePast() {
         // TODO: clock from context/provider
         return isBefore(LocalTime.now());
     }
 
+    @CanIgnoreReturnValue
     public LocalTimeSubject<T> isInThePastOrPresent() {
         // TODO: clock from context/provider
         return isBeforeOrEqualTo(LocalTime.now());
