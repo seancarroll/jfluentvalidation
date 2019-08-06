d06e93ec9d28c75acdacb96d1bd3645cb9291287
seancarroll <seanc28@gmail.com>
2019-08-05 19:34:12 -0500

    fix file name

--- jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/url/HasNoQueryTest.java
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/url/HasNoQueryTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class hasNoQueryTest {
+class HasNoQueryTest {
 
     @Test
     void shouldNotReturnFailureIfActualUrlDoesNotHaveQuery() throws MalformedURLException {
