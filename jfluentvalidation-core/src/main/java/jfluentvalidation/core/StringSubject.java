87ff6eb660a73fbc37996ac905ea19bfc7380f72
seancarroll <seanc28@gmail.com>
2019-08-05 19:32:43 -0500

    remove CanIgnoreReturnValue annotations as I put them on the ComparableSubject interface

--- jfluentvalidation-core/src/main/java/jfluentvalidation/core/StringSubject.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/core/StringSubject.java
@@ -1,6 +1,5 @@
 package jfluentvalidation.core;
 
-import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import jfluentvalidation.constraints.comparable.ComparableConstraints;
 import jfluentvalidation.rules.PropertyRule;
 
@@ -17,74 +16,63 @@ public class StringSubject<T>
         super(StringSubject.class, rule);
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isEqualAccordingToCompareTo(String other) {
         rule.addConstraint(ComparableConstraints.isEqualAccordingToCompareTo(other));
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isNotEqualAccordingToCompareTo(String other) {
         rule.addConstraint(ComparableConstraints.isNotEqualAccordingToCompareTo(other));
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isLessThan(String other) {
         rule.addConstraint(ComparableConstraints.isLessThan(other));
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isLessThanOrEqualTo(String other) {
         rule.addConstraint(ComparableConstraints.isLessThanOrEqualTo(other));
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isGreaterThan(String other) {
         rule.addConstraint(ComparableConstraints.isGreaterThan(other));
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isGreaterThanOrEqualTo(String other) {
         rule.addConstraint(ComparableConstraints.isGreaterThanOrEqualTo(other));
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isBetween(String startInclusive, String endInclusive) {
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isStrictlyBetween(String startExclusive, String endExclusive) {
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isBetween(String start, String end, boolean inclusiveStart, boolean inclusiveEnd) {
         rule.addConstraint(ComparableConstraints.isBetween(start, end, inclusiveStart, inclusiveEnd));
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isNotBetween(String startInclusive, String endInclusive) {
         return myself;
     }
 
-    @CanIgnoreReturnValue
     @Override
     public StringSubject<T> isNotBetween(String start, String end, boolean inclusiveStart, boolean inclusiveEnd) {
         rule.addConstraint(ComparableConstraints.isNotBetween(start, end, inclusiveStart, inclusiveEnd));
