e654116e6632d78446a837196d9d52bfcc9dd5a1
seancarroll <seanc28@gmail.com>
2019-10-14 17:27:23 -0500

    remove stuff that I'm not using

--- jfluentvalidation-core/src/main/java/jfluentvalidation/common/MoreCollections.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/common/MoreCollections.java
@@ -3,10 +3,7 @@ package jfluentvalidation.common;
 import jfluentvalidation.internal.Ensure;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 /**
  *
@@ -31,38 +28,6 @@ public final class MoreCollections {
             return false;
         }
     }
-
-    // gather / collect
-    /**
-     *
-     * @param first
-     * @param second
-     * @param rest
-     * @param <T>
-     * @return
-     */
-    @SafeVarargs
-    public static <T> List<T> accumulate(T first, T second, T... rest) {
-        List<T> items = new ArrayList<>(2 + (rest == null ? 1 : rest.length));
-        items.add(first);
-        items.add(second);
-        // rest is an empty array when the caller provides only two parameters which means that if rest is null
-        // then the caller must have passed in null
-        if (rest == null) {
-            items.add(null);
-        } else {
-            items.addAll(Arrays.asList(rest));
-        }
-        return items;
-    }
-
-//    from assertj
-//    public static <T> T[] prepend(T first, T... rest) {
-//        T[] result = (T[]) new Object[1 + rest.length];
-//        result[0] = first;
-//        System.arraycopy(rest, 0, result, 1, rest.length);
-//        return result;
-//    }
 
     /** Used to avoid http://bugs.sun.com/view_bug.do?bug_id=6558557 */
     static <T> Collection<T> cast(Iterable<T> iterable) {
