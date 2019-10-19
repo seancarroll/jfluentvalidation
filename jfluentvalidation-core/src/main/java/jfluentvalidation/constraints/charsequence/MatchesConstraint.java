7fc8385fe305f0789611e0c890090370f871a78a
seancarroll <seanc28@gmail.com>
2019-10-18 21:57:30 -0500

    remove some TODOs

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/MatchesConstraint.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/charsequence/MatchesConstraint.java
@@ -7,9 +7,6 @@ import jfluentvalidation.validators.RuleContext;
 
 import java.util.regex.Pattern;
 
-// TODO: given we have multiple of these based on type need to either change the name to include the type we are comparing
-// or I guess we could make this take an object and have logic to determine type and perform the appropriate validation
-
 /**
  * Check that the given {@code CharSequence} being validated matches the given regular expression.
  *
