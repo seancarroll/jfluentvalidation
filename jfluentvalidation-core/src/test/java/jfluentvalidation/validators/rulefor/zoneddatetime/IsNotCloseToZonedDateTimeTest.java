6f4b32b839b76aaba8f2f71eddbce4a4277311d0
seancarroll <seanc28@gmail.com>
2019-11-21 17:55:38 -0600

    actually commit tests for IsNotCloseToZonedDateTimeTest

--- jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/zoneddatetime/IsNotCloseToZonedDateTimeTest.java
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/zoneddatetime/IsNotCloseToZonedDateTimeTest.java
@@ -1,8 +1,17 @@
 package jfluentvalidation.validators.rulefor.zoneddatetime;
 
+import jfluentvalidation.ValidationFailure;
+import jfluentvalidation.validators.DefaultValidator;
+import org.junit.jupiter.api.Test;
+
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import static jfluentvalidation.TimeZones.TZ_CHICAGO;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class IsNotCloseToZonedDateTimeTest extends AbstractZonedDateTime {
 
@@ -10,5 +19,78 @@ class IsNotCloseToZonedDateTimeTest extends AbstractZonedDateTime {
         super(ZonedDateTime.of(
             2019, 6, 15, 8, 0, 0, 0,
             TZ_CHICAGO));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenExpectedIsNull() {
+        DefaultValidator<Target> validator = getValidator();
+        assertThrows(NullPointerException.class, () -> validator.ruleForZonedDateTime(Target::getDateTime).isNotCloseTo(null, 1, ChronoUnit.MINUTES, false));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenOffsetUnitIsNull() {
+        DefaultValidator<Target> validator = getValidator();
+        assertThrows(NullPointerException.class, () -> validator.ruleForZonedDateTime(Target::getDateTime).isNotCloseTo(ZonedDateTime.now(), 1, null, false));
+    }
+
+    @Test
+    void shouldNotReturnFailureWhenActualIsNull() {
+        Target t = new Target(null);
+
+        DefaultValidator<Target> validator = getValidator();
+        validator.ruleForZonedDateTime(Target::getDateTime).isNotCloseTo(ZonedDateTime.now(), 1, ChronoUnit.MINUTES, false);
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+
+    @Test
+    void shouldNotReturnFailureWhenOutsideOffset() {
+        Target t = new Target(reference);
+
+        DefaultValidator<Target> validator = getValidator();
+        validator.ruleForZonedDateTime(Target::getDateTime).isNotCloseTo(before, 1, ChronoUnit.HOURS, false);
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+
+    @Test
+    void shouldNotReturnFailureWhenEqualsStrictOffset() {
+        Target t = new Target(reference);
+
+        DefaultValidator<Target> validator = getValidator();
+        validator.ruleForZonedDateTime(Target::getDateTime).isNotCloseTo(before, 1, ChronoUnit.DAYS, true);
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertTrue(failures.isEmpty());
+    }
+    
+    @Test
+    void shouldReturnFailureWhenWithinOffset() {
+        Target t = new Target(reference);
+
+        DefaultValidator<Target> validator = getValidator();
+        validator.ruleForZonedDateTime(Target::getDateTime).isNotCloseTo(before, 2, ChronoUnit.DAYS, false);
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFailureWhenEqualsOffset() {
+        Target t = new Target(reference);
+
+        DefaultValidator<Target> validator = getValidator();
+        validator.ruleForZonedDateTime(Target::getDateTime).isNotCloseTo(before, 1, ChronoUnit.DAYS, false);
+
+        List<ValidationFailure> failures = validator.validate(t);
+
+        assertFalse(failures.isEmpty());
     }
 }
