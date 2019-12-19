ecbfc95a8f859a25612b306a16d8ab8767525af3
seancarroll <seanc28@gmail.com>
2019-12-19 07:58:42 -0600

    adds missing IsCloseTo test for double

---
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/doubles/IsCloseToTest.java
@@ -0,0 +1,140 @@
+package jfluentvalidation.validators.rulefor.doubles;
+
+import jfluentvalidation.ValidationResult;
+import jfluentvalidation.validators.DefaultValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class IsCloseToTest {
+
+    @ParameterizedTest
+    @CsvSource({
+        "1, 1, 1",
+        "1, 2, 10",
+        "-2, 0, 3",
+        "-1, 1, 3",
+        "0, 2, 5"
+    })
+    void shouldNotReturnFailureWhenDifferenceIsLessThanOffset(double actual, double expected, double offset) {
+        Target t = new Target(actual);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForDouble(Target::getNumber).isCloseTo(expected, offset, false);
+
+        ValidationResult validationResult = validator.validate(t);
+
+        assertTrue(validationResult.isValid());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "1, 3, 2",
+        "3, 1, 2",
+        "-2, 0, 2",
+        "-1, 1, 2",
+        "0, 2, 2"
+    })
+    void shouldNotReturnFailureWhenDifferenceIsEqualToOffset(double actual, double expected, double offset) {
+        Target t = new Target(actual);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForDouble(Target::getNumber).isCloseTo(expected, offset, false);
+
+        ValidationResult validationResult = validator.validate(t);
+
+        assertTrue(validationResult.isValid());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "1, 3, 1",
+        "3, 1, 1",
+        "-2, 0, 1",
+        "-1, 1, 1",
+        "0, 2, 1"
+    })
+    void shouldReturnFailureWhenActualIsNotCloseToExpected(double actual, double expected, double offset) {
+        Target t = new Target(actual);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForDouble(Target::getNumber).isCloseTo(expected, offset, false);
+
+        ValidationResult validationResult = validator.validate(t);
+
+        assertFalse(validationResult.isValid());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "1, 2, 1",
+        "3, 2, 1",
+        "-2, -1, 1",
+        "-1, 1, 2",
+        "0, 2, 2"
+    })
+    void shouldReturnFailureWhenDifferenceIsEqualToGivenWithStrictOffset(double actual, double expected, double offset) {
+        Target t = new Target(actual);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForDouble(Target::getNumber).isCloseTo(expected, offset, true);
+
+        ValidationResult validationResult = validator.validate(t);
+
+        assertFalse(validationResult.isValid());
+    }
+
+    @Test
+    void shouldNotThrowWhenActualIsNull() {
+        Target t = new Target(null);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForDouble(Target::getNumber).isCloseTo(0d, 1d, false);
+
+        ValidationResult validationResult = validator.validate(t);
+
+        assertTrue(validationResult.isValid());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenExpectedIsNull() {
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        assertThrows(NullPointerException.class, () -> validator.ruleForDouble(Target::getNumber).isCloseTo(null, 1d, false));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenOffsetIsNull() {
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        assertThrows(NullPointerException.class, () -> validator.ruleForDouble(Target::getNumber).isCloseTo(0d, null, false));
+    }
+
+    @Test
+    void shouldHaveAppropriateErrorMessage() {
+        Target t = new Target(1d);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForDouble(Target::getNumber).isCloseTo(3d, 1d, false);
+
+        ValidationResult validationResult = validator.validate(t);
+
+        assertEquals("number must be close to 3.0 by less than 1.0.", validationResult.getViolations().get(0).getErrorMessage());
+    }
+
+    @Test
+    void shouldHaveAppropriateErrorMessageForStrictOffset() {
+        Target t = new Target(1d);
+
+        DefaultValidator<Target> validator = new DefaultValidator<>(Target.class);
+        validator.ruleForDouble(Target::getNumber).isCloseTo(2d, 1d, true);
+
+        ValidationResult validationResult = validator.validate(t);
+
+        assertEquals("number must be close to 2.0 by strictly less than 1.0.", validationResult.getViolations().get(0).getErrorMessage());
+    }
+
+}
