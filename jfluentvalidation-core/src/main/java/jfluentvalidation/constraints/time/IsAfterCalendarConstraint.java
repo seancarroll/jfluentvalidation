b6644a539b0556674a065618ac82a214b89e2dea
seancarroll <seanc28@gmail.com>
2020-03-01 22:46:31 -0600

    format date in IsAfterCalendar validation message

--- jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/time/IsAfterCalendarConstraint.java
+++ jfluentvalidation-core/src/main/java/jfluentvalidation/constraints/time/IsAfterCalendarConstraint.java
@@ -5,6 +5,7 @@ import jfluentvalidation.constraints.AbstractConstraint;
 import jfluentvalidation.constraints.DefaultMessages;
 import jfluentvalidation.validators.RuleContext;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.function.Supplier;
 
@@ -13,6 +14,9 @@ import java.util.function.Supplier;
  * @param <T>  the target type supported by an implementation.
  */
 public class IsAfterCalendarConstraint<T> extends AbstractConstraint<T, Calendar> {
+
+    // TODO: this format should come from a global option so that users can override
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     private final Supplier<Calendar> other;
 
@@ -37,7 +41,7 @@ public class IsAfterCalendarConstraint<T> extends AbstractConstraint<T, Calendar
             // doing this here instead of using addParametersToContext because there are instances were we are
             // getting a supplier for the current date/time and if we do it in addParametersToContext we will
             // get a slightly different values than what we used for the comparison
-            context.getMessageContext().appendArgument("other", otherValue);
+            context.getMessageContext().appendArgument("other", FORMAT.format(otherValue.getTime()));
         }
 
         return isAfter;
