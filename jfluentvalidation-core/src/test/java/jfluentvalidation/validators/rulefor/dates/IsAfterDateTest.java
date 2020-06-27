d8ee99dd18bce5e02414ceb92d37fe7db2baec07
seancarroll <seanc28@gmail.com>
2020-06-26 22:05:24 -0500

    remove hibernate test

--- jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/dates/IsAfterDateTest.java
+++ jfluentvalidation-core/src/test/java/jfluentvalidation/validators/rulefor/dates/IsAfterDateTest.java
@@ -4,14 +4,8 @@ import jfluentvalidation.ValidationResult;
 import jfluentvalidation.validators.DefaultValidator;
 import org.junit.jupiter.api.Test;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.Date;
-import java.util.Set;
 
 import static jfluentvalidation.TimeZones.TZ_CHICAGO;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -25,21 +19,6 @@ class IsAfterDateTest extends AbstractDateTest {
             2019, 8, 7, 9, 0, 0, 0,
             TZ_CHICAGO)
         );
-    }
-
-    // TODO: delete
-    @Test
-    void hibernateValidator() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-
-        Instant instant = Instant.now();
-        Date d = Date.from(instant);
-        Target t = new Target(d);
-
-        Set<ConstraintViolation<Target>> constraints = validator.validate(t);
-
-        System.out.println(constraints);
     }
 
     @Test
